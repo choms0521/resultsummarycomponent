6f34ac99303fc0d394d5b2a4ada723d529cb6edc
choms0521 <choms0521@gmail.com>
2023-11-23 17:50:02 +0900

    finish

---
+++ index.js
@@ -0,0 +1,28 @@
+function drawSummary(jsonData) {
+  const liElements = document.querySelectorAll(".summary > ul > li");
+
+  console.log(jsonData);
+  liElements.forEach((elem) => {
+    const data = jsonData.filter(
+      (value) => value.category.toLowerCase() === elem.className
+    );
+
+    console.log(data[0].icon)
+
+    if (data) {
+      elem.innerHTML = `
+        <img src=\"${data[0].icon}\"> 
+        <p>${data[0].category}</p>
+        <p><span>${data[0].score}</span> / 100</p>
+
+     `;
+    }
+
+  });
+}
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((json) => {
+    drawSummary(json);
+  });
